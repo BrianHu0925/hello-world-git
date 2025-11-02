047ac3bbff627182afb19c67c58e8947f98288a8
BrianHu0925 <j100376@my.cmsh.cyc.edu.tw>
2025-11-02 23:30:19 +0800

    update hello_world.cpp in the branch banana

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,5 +4,6 @@ int main() {
     cout << "Hello, World!" << endl;
     cout << "Hello, github.com!" << endl;
     cout << "This is the branch main!" << endl;
+    cout << "This is the branch banana!" << endl;
     return 0;
 }
